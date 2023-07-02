2b53579c68f62a00b7147d0b7d306ca7300b2c18
Nil <nilbasora@gmail.com>
2023-07-03 00:25:06 +0200

    basic setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='GPTDriver',
+    packages=find_packages(include=['GPTDriver']),
+    version='0.1.0',
+    description='Selenium Driver to Control ChatGPT from Command Lines',
+    author='Nil Basora',
+    author_email="nilbasora@gmail.com",
+    url="https://github.com/nilbasora/GPTDriver",
+    keywords=['GPT', 'OpenAI', 'ChatGPT', 'Selenium', 'Driver'],
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+  ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.4.0'],
+    test_suite='tests',
+)
