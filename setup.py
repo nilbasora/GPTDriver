385a38b96b59b716ba77f6e61ca12490b7c9e6c3
Nil <nilbasora@gmail.com>
2023-07-06 20:07:59 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -10,15 +10,13 @@ setup(
     url="https://github.com/nilbasora/GPTDriver",
     keywords=['GPT', 'OpenAI', 'ChatGPT', 'Selenium', 'Driver'],
     license='MIT',
-    install_requires=[],
+    install_requires=['undetected-chromedriver==3.5.0',
+                      'selenium==4.10.0'],
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
-        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
